6668e369409eaa2e4d3824d092300af89f106b7a
Ramon <ramonfrombr@gmail.com>
2024-03-23 04:48:06 -0300

    Defined sample chart to display in admin dashboard

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,11 +6,11 @@ class AdminController < ApplicationController
     @orders = Order.where(fulfilled: false).order(created_at: :desc).take(5)
     @quick_stats = {
       sales: Order.where(created_at: Time.now.midnight..Time.now).count,
-      revenue: Order.where(created_at: Time.now.midnight..Time.now).sum(:total).round(),
-      avg_sale: Order.where(created_at: Time.now.midnight..Time.now).average(:total).round(),
+      revenue: Order.where(created_at: Time.now.midnight..Time.now).sum(:total)&.round(),
+      avg_sale: Order.where(created_at: Time.now.midnight..Time.now).average(:total)&.round(),
       per_sale: OrderProduct.joins(:order).where(orders: {
                                                    created_at: Time.now.midnight..Time.now,
-                                                 }).average(:quantity),
+                                                 })&.average(:quantity),
     }
     @orders_by_day = Order.where("created_at > ?", Time.now - 7.days).order(:created_at)
     @orders_by_day = @orders_by_day.group_by { |order| order.created_at.to_date }
