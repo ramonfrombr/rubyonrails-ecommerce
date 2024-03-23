5f468de26e8d7d7a027fab5b780f8cf00a291a52
Ramon <ramonfrombr@gmail.com>
2024-03-23 05:38:59 -0300

    Dashboard revenue chart uses the real data about sales by passing to dashboard controller javascript

--- app/javascript/controllers/dashboard_controller.js
+++ app/javascript/controllers/dashboard_controller.js
@@ -3,11 +3,12 @@ import { Chart, registerables } from 'chart.js'
 
 Chart.register(...registerables)
 
-// Connects to data-controller="dashboard"
 export default class extends Controller {
-  connect() {
-    const data = [10, 20, 30, 40, 50, 60, 70]
-    const labels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+  static values = { revenue: Array }
+
+  initialize() {
+    const data = this.revenueValue.map((item) => item[1] / 100.0)
+    const labels = this.revenueValue.map((item) => item[0])
 
     const ctx = document.getElementById('revenueChart')
 
