da2b8b967c8ea7833f90076d58558320a806c104
Ramon <ramonfrombr@gmail.com>
2024-03-28 02:47:36 -0300

    Defined addToCart() and selectSize() methods in stimulus products controller

--- app/javascript/controllers/products_controller.js
+++ app/javascript/controllers/products_controller.js
@@ -2,7 +2,13 @@ import { Controller } from '@hotwired/stimulus'
 
 // Connects to data-controller="products"
 export default class extends Controller {
-  connect() {
-    console.log('Connected controller')
+  addToCart() {
+    console.log('product: ', this.productValue)
+  }
+
+  selectSize(e) {
+    this.sizeValue = e.target.value
+    const selectedSizeEl = document.getElementById('selected-size')
+    selectedSizeEl.innerText = `Selected Size: ${this.sizeValue}`
   }
 }
