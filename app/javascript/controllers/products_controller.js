63430b17097eadbe9122364e00e018451d3e732f
Ramon <ramonfrombr@gmail.com>
2024-03-28 02:59:32 -0300

    Defined static values in stimulus products controller. Defined action for "Add to Cart" button.

--- app/javascript/controllers/products_controller.js
+++ app/javascript/controllers/products_controller.js
@@ -2,6 +2,8 @@ import { Controller } from '@hotwired/stimulus'
 
 // Connects to data-controller="products"
 export default class extends Controller {
+  static values = { size: String, product: Object }
+
   addToCart() {
     console.log('product: ', this.productValue)
   }
